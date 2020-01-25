5b03a9e8c308d4dd40f54f8b11221d2124353471
urciuolim <urciuolim@gmail.com>
2020-01-25 10:45:04 -0500

    Arrays - Left Rotation

---
+++ src/Arrays/LeftRotation.java
@@ -0,0 +1,57 @@
+package Arrays;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LeftRotation {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] val = new int[a.length];
+        int counter = 0;
+        for (int i = d; i < a.length; i++)
+            val[counter++] = a[i];
+        for (int i = 0; i < d; i++)
+            val[counter++] = a[i];
+        return val;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
